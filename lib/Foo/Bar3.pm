2a801bbc3a38b50f4a52a35ffcde6a5707573062
brian d foy <brian.d.foy@gmail.com>
2021-12-28 15:42:41 -0500

    Initial creation with bmt

---
+++ lib/Foo/Bar3.pm
@@ -0,0 +1,65 @@
+package Foo::Bar3;
+use strict;
+
+use warnings;
+no warnings;
+
+our $VERSION = '0.001_01';
+
+=encoding utf8
+
+=head1 NAME
+
+Foo::Bar3 - The foo bar
+
+=head1 SYNOPSIS
+
+	use Foo::Bar3;
+
+=head1 DESCRIPTION
+
+=over 4
+
+=item new
+
+=cut
+
+sub new {
+
+	}
+
+=item init
+
+=cut
+
+sub init {
+
+	}
+
+=back
+
+=head1 TO DO
+
+
+=head1 SEE ALSO
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+	http://github.com/briandfoy/foo-bar3
+
+=head1 AUTHOR
+
+brian d foy, C<< <brian d foy> >>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright © 2021, brian d foy, All Rights Reserved.
+
+You may redistribute this under the terms of the Artistic License 2.0.
+
+=cut
+
+1;
